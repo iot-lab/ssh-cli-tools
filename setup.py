7c8e6c0293cb569a54793a6e677eaf7844e009fc
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-10-08 13:01:16 +0200

    tox: add env for python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  'Intended Audience :: End Users/Desktop',
                  'Environment :: Console',
                  'Topic :: Utilities', ],
